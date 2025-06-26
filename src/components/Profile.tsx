26a4ab742544d21cdb44f9cdc9837ca75336b64e
madas02d <mb.damascene24@gmail.com>
2025-06-26 15:47:42 +0200

    update CV

--- src/components/Profile.tsx
+++ src/components/Profile.tsx
@@ -19,13 +19,13 @@ In addition, I have hands-on experience in IT support, including troubleshooting
         <div className="btn-container">
           <button
             className="btn btn-color-2"
-            onClick={() => window.open('/assets/CV.pdf')}
+            onClick={() => window.open('/assets/Damascene-CV.pdf')}
           >
             Download CV
           </button>
           <button
             className="btn btn-color-2"
-            onClick={() => window.open('/assets/Lebenslauf.pdf')}
+            onClick={() => window.open('/assets/Damascene-Lebenslauf.pdf')}
           >
             Download Germany CV
           </button>
